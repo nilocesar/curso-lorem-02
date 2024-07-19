75b047abc4c03fb748aa29dd165126bc5055265b
nilo.cesar <nilo.cesar@afferolab.com.br>
2024-07-19 17:06:31 -0300

    fix: remove a dist git

---
+++ dist/app/config.js
@@ -0,0 +1,35 @@
+var config = {
+    salvarDados: true,
+    debug: false,
+    waterMark: false,
+    language: 'pt-br',
+    lms: {
+        name: 'default'
+    },
+    acessibility: {
+        tools: false,
+        outlines: false,
+        vlibras: false,
+        customLibras: false
+    },
+    behaviors: {
+        adaptive: false,
+        width: 1920,
+        height: 1080,
+        fontSize: 30
+    },
+    modalVoltar: {
+        active: false,
+        msg: 'Você quer continuar de onde parou ou reiniciar o curso?',
+        yes: 'CONTINUAR',
+        no: 'REINICIAR',
+        color: '#0a698d'
+    },
+    pages: [{
+        uid: 'onepag01',
+        src: 'onepag01/index.html'
+    }]
+};
+try {
+    module.exports = config;
+} catch (e) {}
